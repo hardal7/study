6e172511406e5692949ae50385423a3e1c159c36
hardal <hardal7@protonmail.com>
2025-12-02 20:37:56 +0300

    Revise session struct definitions & create migration accordingly

--- internal/model/session.go
+++ internal/model/session.go
@@ -3,19 +3,14 @@ package model
 import "time"
 
 type Session struct {
-	ID        int           `db:"id"`
-	Name      string        `db:"name"`
-	Password  string        `db:"password"`
-	Admin     User          `db:"admin"`
-	Users     []SessionUser `db:"users"`
-	Expiry    time.Time     `db:"expiry"`
-	CreatedAt time.Time     `db:"created_at"`
-	UpdatedAt time.Time     `db:"updated_at"`
-}
-
-type SessionUser struct {
-	ID     int `db:"id"`
-	UserID int `db:"userid"`
+	ID        int       `db:"id"`
+	Name      string    `db:"name"`
+	Password  string    `db:"password"`
+	Admin     int       `db:"admin_id"`
+	Users     []int     `db:"user_ids"`
+	Expiry    time.Time `db:"expiry"`
+	CreatedAt time.Time `db:"created_at"`
+	UpdatedAt time.Time `db:"updated_at"`
 }
 
 type CreateSessionRequest struct {
@@ -24,10 +19,16 @@ type CreateSessionRequest struct {
 	Expiry   int    `json:"expiry"`
 }
 
+type JoinSessionRequest struct {
+	SessionID string `json:"session_id"`
+	Password  string `json:"password"`
+}
+
 type EditSessionRequest struct {
-	NewName       string        `json:"name"`
-	NewPassword   string        `json:"password"`
-	NewExpiry     int           `json:"expiry"`
-	RemovedUsers  []SessionUser `json:"removed_users"`
-	DeleteSession int           `json:"delete"`
+	SessionID     int      `json:"id"`
+	NewName       string   `json:"name"`
+	NewPassword   string   `json:"password"`
+	NewExpiry     int      `json:"expiry"`
+	RemovedUsers  []string `json:"removed_users"`
+	DeleteSession int      `json:"delete"`
 }
